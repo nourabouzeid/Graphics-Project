d5b4c656f22077b57db20d0b5160c11a592950cd
Mariam Amin <128838373+Mariam-Amin12@users.noreply.github.com>
2025-04-26 23:07:45 +0300

    new verion of map

--- config/generate_lands.py
+++ config/generate_lands.py
@@ -1,9 +1,9 @@
 import json
 import random
 
-ISLAND_SIZE = 16
-width = 12
-height = 16
+ISLAND_SIZE = 20
+width = 4
+height = 4
 NUM_ISLANDS = random.randint(4, 7)
 DIRECTIONS = ['left', 'up', 'right']
 
@@ -18,68 +18,64 @@ DIR_VECTORS = {
 
 def round_vector(vec, decimals=1):
     return [round(x, decimals) for x in vec]
-
 def generate_valid_path(num_islands):
-    seed=2
-    last=3
-    m=17
+    seed = 2
+    last = 3
+    m = 17
     path = []
     prev = None
     for _ in range(num_islands - 1):
-
         while True:
-            glc= seed *last +m
+            glc = seed * last + m
             seed = glc % 3
-            last=last + 1
+            last = last + 1
             if last > 100:
-                last=0
-            
+                last = 0
+
             dir = random.choice(DIRECTIONS)
-            
-            if prev == 'left' and dir == 'right':
+
+            if prev == 'left' and dir in ['left', 'right']:
                 continue
-            if prev == 'right' and dir == 'left':
-                    
+            if prev == 'right' and dir in ['left', 'right']:
                 continue
-            else :
+            if prev == 'up' and dir == 'up':
+                continue
+            else:
                 break
-      
-        
+
         path.append(dir)
         prev = dir
     return path
 
+
 def island_position_from_path(path):
-    pos = [(6, 0, 6)]
-    current = (6, 0, 6)
+    pos = [(0, 0, 0)]
+    current = (0, 0, 0)
 
 
 
     for i in range(len(path)):
         if path[i] == 'left':
-            if i > 0 and path[i - 1] == 'left':
-                current = (current[0] - 2 - width, current[1], current[2])
+            if i > 2 and path[i - 3] == 'right':
+                current = (current[0] - 4 - width, current[1], current[2])
             else:
                 current = (current[0] - 2 - height, current[1], current[2])
         elif path[i] == 'right':
-            if i > 0 and path[i - 1] == 'right':
-                current = (current[0] + 2 + width, current[1], current[2])
+            if i > 2 and path[i - 3] == 'left':
+                current = (current[0] + 4 + width, current[1], current[2])
             else:
                 current = (current[0] + 2 + height, current[1], current[2])
         else:
-            if i > 0 and path[i - 1] == 'up':
-                current = (current[0], current[1], current[2] + 2 + width)
-            else:
-                current = (current[0], current[1], current[2] + 2 + height)
+            current = (current[0], current[1], current[2] + 2 + height)
 
         pos.append(current)
     return pos
 
 def random_trap_velocity():
     directions = [
-        (1, 0, 0), (-1, 0, 0),
-        (0, 0, 1), (0, 0, -1),
-        (1, 0, 1), (-1, 0, -1), (1, 0, -1), (-1, 0, 1)
+        (0.5, 0, 0), (-0.5, 0, 0),
+        (0, 0, 0.5), (0, 0, -0.5),
+        (0.5, 0, 0.5), (-0.5, 0, -0.5), (0.5, 0, -0.5), (-0.5, 0, 0.5)
     ]
     return random.choice(directions)
 
@@ -88,16 +84,16 @@ def generate_traps_for_island(base_pos):
     num_traps = random.randint(1, 3)
     for _ in range(num_traps):
         offset = (
-            random.uniform(1, ISLAND_SIZE - 4),
+            random.uniform(.1,  .4),
             1,
-            random.uniform(1, ISLAND_SIZE - 4)
+            random.uniform(.1,  .4)
         )
-        pos = [base_pos[0] + offset[0], base_pos[1] + offset[1], base_pos[2] + offset[2]]
+        pos = [base_pos[0]-1 , -1, base_pos[2] +1]
         vx, vy, vz = random_trap_velocity()
         trap = {
             "position": round_vector(pos),
             "rotation": [0, 0, 0],
-            "scale": [3, 3, 3],
+            "scale": [1, 1, 1],
             "components": [
                 {
                     "type": "Mesh Renderer",
@@ -108,10 +104,10 @@ def generate_traps_for_island(base_pos):
                 #     "type": "Movement",
                 #     "linearVelocity": [vx, vy, vz],
                 #     "angularVelocity": [0, 0, 0],
-                #     # "boundaries": {
-                #     #     "x": round_vector([base_pos[0], base_pos[0] + ISLAND_SIZE]),
-                #     #     "z": round_vector([base_pos[2], base_pos[2] + ISLAND_SIZE])
-                #     # }
+                #     "boundaries": {
+                #         "x": round_vector([0.5, 0.5]),
+                        
+                #     }
                 # }
             ]
         }
@@ -127,7 +123,7 @@ def generate_scene():
 
     # Camera
     world.append({
-        "position": [-10, 15, 0],
+        "position": [0, 0, 0],
         "rotation": [-20, -90, 0],
         "scale": [1, 1, 1],
         "components": [
@@ -138,9 +134,9 @@ def generate_scene():
 
     # Character
     world.append({
-        "position": round_vector([1, 1, 20]),
+        "position": round_vector([0, -1, 0]),
         "rotation": [0, 180, 0],
-        "scale": [3.5, 3.5, 3.5],
+        "scale": [1, 1, 1],
         "components": [
             {
                 "type": "Mesh Renderer",
@@ -152,12 +148,12 @@ def generate_scene():
 
     # Ground, box, key, and traps
     for i, pos in enumerate(positions):
-        ground_pos = [pos[0] + ISLAND_SIZE / 2, 0, pos[2] + ISLAND_SIZE / 2]
-
+       
         # Ground
         world.append({
-            "position": round_vector(ground_pos),
-            "rotation": [180, 0, 0],
+            "position": round_vector(pos),
+            "rotation": [0, 0, 0],
+            "scale": [8, 8, 8],
             "components": [
                 {
                     "type": "Mesh Renderer",
@@ -169,10 +165,11 @@ def generate_scene():
 
         if i < len(positions) - 1:
             # Box
-            box_pos = [pos[0] + 5, 5.5, pos[2] + 5]
+            box_pos = [pos[0] , .3, pos[2] ]
             world.append({
                 "position": round_vector(box_pos),
                 "rotation": [0, 0, 0],
+                "scale": [0.3, 0.3, 0.3],
                 "components": [
                     {
                         "type": "Mesh Renderer",
@@ -181,13 +178,13 @@ def generate_scene():
                     }
                 ]
             })
-        else:
+        if i == len(positions) - 1:
             # Key
-            key_pos = [pos[0], pos[1] +3.5, pos[2] ]
+            key_pos = [pos[0], pos[1]-0.2, pos[2]]
             world.append({
                 "position": round_vector(key_pos),
                 "rotation": [0, 0, 0],
-                "scale": [0.6, 0.6, 1],
+                "scale": [0.2, 0.2, .3],
                 "components": [
                     {
                         "type": "Mesh Renderer",
@@ -230,14 +227,14 @@ def generate_scene():
             },
             "textures": {
                 "character": "assets/textures/colormap.png",
-                "groundEarth": "assets/textures/1356119798126.png",
+                "groundEarth": "assets/textures/ground_new4.jpg",
                 "box": "assets/textures/1335133419679.png",
                 "key": "assets/textures/gold.png",
                 "trap": "assets/textures/trap.png"
             },
             "meshes": {
                 "character": "assets/models/character-male-a.obj",
-                "groundEarth": "assets/models/minecraft.obj",
+                "groundEarth": "assets/models/ground_new4.obj",
                 "box": "assets/models/box.obj",
                 "key": "assets/models/key.obj",
                 "trap": "assets/models/trap.obj"
